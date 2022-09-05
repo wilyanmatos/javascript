e86a4a93ec6edf87a68eb1a5864bbf1032ca44b0
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-09-05 16:32:31 -0300

    Novo Exercicio
    Estrutura de condição

---
+++ Exercicios/Aula 04 - Estrutura de Repetição/Ex01.js
@@ -0,0 +1,6 @@
+var velo = 50.5
+console.log('A velocidade do seu carro é  ${velo}km/h')
+if(velo > 50){
+    conlose.log('Você ultrapassou o limite. Multado')
+}
+console.log('Digira sempre usandu cinto de segurança')
