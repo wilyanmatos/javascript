e72fddc60d85022752ea44835d76cb119f72a8bd
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-09-14 16:05:42 -0300

    Novo Desafio
    Relogio em tempo real que muda imagem

--- Desafios/Desafio aula 4/semana.js
+++ Desafios/Desafio aula 4/semana.js
@@ -1,6 +1,6 @@
-min - 23:50
+
 var agora = new Date()
-var AtualSem = agora.getDay()
+var AtualSem = agora.getDate()
 console.log(AtualSem)
 /*
 0 - Domingo
@@ -13,10 +13,28 @@ console.log(AtualSem)
 */
 switch(AtualSem){
 
-case 3:
-    console.log("Quinta")
-    break
-default:
-    console.log('Invalido')
+    case 0:
+        console.log("Domingo")
+        break
+    case 1:
+        console.log("Segunda")
+        break
+    case 2:
+        console.log("Terça")
+        break
+    case 3:
+        console.log("Quarta")
+        break
+    case 4:
+        console.log("Quinta")
+        break
+    case 5:
+        console.log("Sexta")
+        break
+    case 6:
+        console.log("Sabado")
+        break
+    default:
+        console.log(' {ERRO} - Invalido')
 
 }
