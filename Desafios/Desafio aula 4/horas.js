b1deba74c8a6853197541fd21f433b17dc359278
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-09-19 15:38:27 -0300

    Desafio Completo

--- Desafios/Desafio aula 4/horas.js
+++ Desafios/Desafio aula 4/horas.js
@@ -9,5 +9,5 @@ else if(horas < 12 ){
 }else if(horas <= 17){
     console.log("Boa Tarde!")
 }else{
-    console.log("Boa Noite!")
+    alert("Boa Noite!")
 }
--- Desafios/Desafio aula 4/script.js
+++ Desafios/Desafio aula 4/script.js
@@ -1,11 +1,35 @@
-var info_date = new Date()
-var horas = info_date.getHours()
-var min = info_date.getMinutes()
-var bg = document.getElementById("principal")
-var time = document.getElementById("time")
-time.innerHTML = `<p> Agora são ${horas}:${min} Horas</p>`
+function carregar(){
+    var info_date = new Date()
+    var min = info_date.getMinutes()
+    var bg = window.document.getElementById("principal")
+    var time = window.document.getElementById("time")
+    var periodo = window.document.getElementById("img_periodo")
+    var horas = info_date.getHours()
+    var saudacao;
 
-if(nome == "Wilson"){
-    bg.style.background = "black"
+    //BOA MADRUGADA!!
+    if(horas >= 0 && horas < 6){
+        periodo.src = "imgs/Madrugada.jpg"
+        bg.style.background = "rgb(86, 86, 247)"
+        saudacao = "Boa Madrugada!"
+
+    //BOM DIA!!
+    }else if (horas >= 6 && horas < 12){
+        periodo.src = "imgs/Manhã.png"
+        saudacao = "Bom Dia!"
+
+    //BOA TARDE!!
+    }else if(horas >=12 && horas < 17){
+        periodo.src = "imgs/tarde.jpg"
+        saudacao = "Boa Tarde!"
+
+    //BOA NOITE!!
+    }else if(horas >= 17 && horas < 22){
+        periodo.src = "imgs/noite.jpg"
+        saudacao = "Boa Noite!"
+    }
+
+    time.innerHTML = `<p> Agora são ${horas}:${min} Horas. <br>  ${saudacao}</p>`
+
 }
 
