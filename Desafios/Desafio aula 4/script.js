bb612b906b3ffc89c68bdb268d6b24f47403a0b1
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-09-30 10:02:50 -0300

    Novo Desafio

--- Desafios/Desafio aula 4/script.js
+++ Desafios/Desafio aula 4/script.js
@@ -1,4 +1,5 @@
 function carregar(){
+    var nome = window.prompt("Olá, qual o seu nome?")
     var info_date = new Date()
     var min = info_date.getMinutes()
     var bg = window.document.getElementById("principal")
@@ -10,23 +11,26 @@ function carregar(){
     //BOA MADRUGADA!!
     if(horas >= 0 && horas < 6){
         periodo.src = "imgs/Madrugada.jpg"
-        bg.style.background = "rgb(86, 86, 247)"
-        saudacao = "Boa Madrugada!"
+        bg.style.backgroundColor = '#07416B'
+        saudacao = `Boa Madrugada, ${nome}`
 
     //BOM DIA!!
     }else if (horas >= 6 && horas < 12){
         periodo.src = "imgs/Manhã.png"
-        saudacao = "Bom Dia!"
+        bg.style.backgroundColor = "#ECA555"
+        saudacao = `Bom Dia, ${nome}`
 
     //BOA TARDE!!
     }else if(horas >=12 && horas < 17){
         periodo.src = "imgs/tarde.jpg"
-        saudacao = "Boa Tarde!"
+        bg.style.backgroundColor = "#E07714"
+        saudacao = `Boa Tarde, ${nome}`
 
     //BOA NOITE!!
     }else if(horas >= 17 && horas < 22){
         periodo.src = "imgs/noite.jpg"
-        saudacao = "Boa Noite!"
+        bg.style.backgroundColor = "#0B004D"
+        saudacao = `Boa Noite, ${nome}`
     }
 
     time.innerHTML = `<p> Agora são ${horas}:${min} Horas. <br>  ${saudacao}</p>`
