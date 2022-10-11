52acd8cfc76f1fb7451c9abce1b8baee3a72a30f
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-10-11 16:53:23 -0300

    Nova desafio
    Verificador de idade

---
+++ Desafios/Desafio-2-aula-4/back.js
@@ -0,0 +1,6 @@
+/*Pegar o valor do RADIO */
+function btn(){
+    var nasc = window.document.getElementById("nascimento")
+    var sex = window.document.querySelectorAll("input[name='sexo']")
+    window.alert(sex[1])
+}
