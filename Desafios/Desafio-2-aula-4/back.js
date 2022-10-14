60d88216520da2bc06ec28ff1dfbc8b3c347ef7e
Wilyan <92753649+wilyanmatos@users.noreply.github.com>
2022-10-14 16:09:32 -0300

    Mundanças
    Algumas alterações mínimas para o projeto

--- Desafios/Desafio-2-aula-4/back.js
+++ Desafios/Desafio-2-aula-4/back.js
@@ -1,6 +1,20 @@
-/*Pegar o valor do RADIO */
+
 function btn(){
-    var nasc = window.document.getElementById("nascimento")
-    var sex = window.document.querySelectorAll("input[name='sexo']")
-    window.alert(sex[1])
+    var nasc = window.document.getElementById("nascimento").value
+    var sexo = document.querySelector("input[name='sexo']:checked").value
+    var idade = Number(2022 - nasc)
+    var div_msg = document.getElementsByClassName("div_img")
+    var img = document.getElementById("img")
+    var msg = document.getElementById("msg")
+    img.innerHTML = style="opacity:1;"
+    div_msg.innerHTML = style="opacity:1;"
+    if (idade >= 0 && idade < 6){
+        img.src = "Photos/Homens/bb_knife.jpg"
+    }else if(idade >= 6 && idade < 15){
+        img.src = "Photos/Homens/Menino.jpg"
+    }else{
+        window.alert("ERRO")
+    }
+    msg.innerText = `Detectamos um(a) ${sexo} de ${idade} anos`
+    
 }
